511674e0edf4246ae165a8429198fb3d24daf4a5
Miguel Rodriguez <miguekos1233@gmail.com>
2020-11-12 00:13:58 -0500

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row">
+      <div class="col-12 col-md-2"></div>
+      <div class="col-12 col-md-8">
+        <q-form @submit="onSubmit" class="q-gutter-md">
+          <div class="row q-gutter-xs">
+            <div class="col">
+              <q-input
+                name="nombres"
+                v-model="nombres"
+                color="primary"
+                label="Nombres"
+                filled
+                clearable
+                lazy-rules
+                :rules="[val => (val && val.length > 0) || 'Campo obligatorio']"
+              />
+            </div>
+            <div class="col">
+              <q-input
+                name="apellidos"
+                v-model="apellidos"
+                color="primary"
+                label="Apellidos"
+                filled
+                clearable
+                lazy-rules
+                :rules="[val => (val && val.length > 0) || 'Campo obligatorio']"
+              />
+            </div>
+          </div>
+
+          <div class="row q-gutter-xs">
+            <div class="col">
+              <q-input
+                name="documentIdentidad"
+                v-model="documentIdentidad"
+                color="primary"
+                label="DNI/CE/RUC"
+                filled
+                maxlength="12"
+                counter
+                clearable
+                lazy-rules
+                :rules="[val => (val && val.length > 0) || 'Campo obligatorio']"
+              />
+            </div>
+            <div class="col">
+              <q-input
+                name="teleCelular"
+                v-model="teleCelular"
+                color="primary"
+                label="Telefono/Celular"
+                filled
+                maxlength="9"
+                counter
+                clearable
+                lazy-rules
+                :rules="[val => (val && val.length > 0) || 'Campo obligatorio']"
+              />
+            </div>
+          </div>
+
+          <div class="row q-gutter-xs">
+            <div class="col">
+              <q-input
+                name="direccion"
+                v-model="direccion"
+                color="primary"
+                label="Direccion"
+                filled
+                clearable
+                lazy-rules
+                :rules="[val => (val && val.length > 0) || 'Campo obligatorio']"
+              />
+            </div>
+            <div class="col">
+              <q-select
+                name="metodosDePagos"
+                filled
+                v-model="metodosDePagos"
+                :options="options"
+                label="Metodos de pagos"
+                clearable
+                lazy-rules
+                :rules="[val => (val && val.length > 0) || 'Campo obligatorio']"
+              />
+            </div>
+          </div>
+
+          <div align="center">
+            <q-btn label="Enviar" type="submit" color="primary" />
+          </div>
+        </q-form>
+        <q-card
+          v-if="submitResult.length > 0"
+          flat
+          bordered
+          class="q-mt-md bg-grey-2"
+        >
+          <q-card-section
+            >Submitted form contains the following formData (key =
+            value):</q-card-section
+          >
+          <q-separator />
+          <q-card-section class="row q-gutter-sm items-center">
+            <div
+              v-for="(item, index) in submitResult"
+              :key="index"
+              class="q-px-sm q-py-xs bg-grey-8 text-white rounded-borders text-center text-no-wrap"
+            >
+              {{ item.name }} = {{ item.value }}
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-12 col-md-2"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      options: ["Deposito", "TDC"],
+      name: "Jane Doe",
+      nombres: "",
+      apellidos: "",
+      documentIdentidad: "",
+      teleCelular: "",
+      direccion: "",
+      metodosDePagos: "",
+      submitResult: []
+    };
+  },
+
+  methods: {
+    onSubmit(evt) {
+      const formData = new FormData(evt.target);
+      const submitResult = [];
+
+      for (const [name, value] of formData.entries()) {
+        submitResult.push({
+          name,
+          value
+        });
+      }
+
+      this.submitResult = submitResult;
+    }
+  }
+};
+</script>
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="q-pt-xs">
+      <Form />
+    </div>
+    <div class="q-pt-xs">
+      <Table />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  components: {
+    Form: () => import("components/Form"),
+    Table: () => import("components/Table")
+  }
+};
+</script>
